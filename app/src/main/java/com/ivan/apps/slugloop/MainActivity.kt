554ff21875d2e64424d7df1efb3169081901e9ad
Ivan Alvarado <ivalvara21@gmail.com>
2019-09-15 21:07:10 -0700

    add google maps key through gradle.properties file

---
+++ app/src/main/java/com/ivan/apps/slugloop/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.ivan.apps.slugloop
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
