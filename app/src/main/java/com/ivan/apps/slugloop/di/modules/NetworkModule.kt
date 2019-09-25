e641774cd4436ef96ff850a179a7ba9463c02349
Ivan Alvarado <ivalvara21@gmail.com>
2019-09-24 21:43:54 -0700

    provide network dependencies through network module

---
+++ app/src/main/java/com/ivan/apps/slugloop/di/modules/NetworkModule.kt
@@ -0,0 +1,25 @@
+package com.ivan.apps.slugloop.di.modules
+
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
+import dagger.Module
+import dagger.Provides
+import okhttp3.OkHttpClient
+import javax.inject.Singleton
+
+@Module
+class NetworkModule {
+
+    @Provides
+    @Singleton
+    internal fun provideGson(): Gson {
+        val gsonBuilder = GsonBuilder()
+        return gsonBuilder.create()
+    }
+
+    @Provides
+    @Singleton
+    fun provideOkHttpClient(): OkHttpClient {
+        return
+    }
+}
--- app/src/main/java/com/ivan/apps/slugloop/repository/BusRepository.kt
+++ app/src/main/java/com/ivan/apps/slugloop/repository/BusRepository.kt
@@ -1,13 +1,15 @@
 package com.ivan.apps.slugloop.repository
 
 import com.ivan.apps.slugloop.network.Bus
+import com.ivan.apps.slugloop.network.BusService
 import io.reactivex.Observable
+import javax.inject.Inject
 
 interface BusRepository {
     fun getBuses(): Observable<Bus>
 }
 
-class BusRepositoryImpl : BusRepository {
+class BusRepositoryImpl @Inject constructor(private val busService: BusService) : BusRepository {
     override fun getBuses(): Observable<Bus> {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
